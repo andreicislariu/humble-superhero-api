4082b1070996598ab40cd0eab609f8d1a5cd3338
andreiFC <andreicislariu@gmail.com>
2025-02-09 14:31:49 +0200

    feat: implement superhero service with in-memory storage and sorting

---
+++ services/superhero.service.ts
@@ -0,0 +1,25 @@
+import { Superhero, CreateSuperheroDto } from "../models/superhero.model";
+
+export class SuperheroService {
+  private superheroes: Superhero[] = [];
+
+  addSuperhero(superheroData: CreateSuperheroDto): Superhero {
+    const newSuperhero: Superhero = {
+      id: Date.now().toString(),
+      ...superheroData,
+    };
+
+    this.superheroes.push(newSuperhero);
+    return newSuperhero;
+  }
+  getSuperheroes(): Superhero[] {
+    return [...this.superheroes].sort(
+      (a, b) => b.humilityScore - a.humilityScore
+    );
+  }
+  getSuperheroesByid(id: string): Superhero | undefined {
+    return this.superheroes.find((hero) => hero.id === id);
+  }
+}
+
+export const superheroService = new SuperheroService();
